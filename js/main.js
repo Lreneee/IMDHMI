349ed70055160203584af0f648e13a431ff2ad80
Lreneee <irenedekoning1@gmail.com>
2022-01-08 15:14:43 +0100

    Opzet scherm :)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 const desk1 = document.getElementById("desk1");
 const popup1 = document.getElementById("popup1");
 
+let popup1Visible = false;
 desk1.onmouseenter = () => {
-    setTimeout(showPopup, 2000);
-
+    setTimeout(showPopup, 500);
+}
+desk1.onmouseleave = () => {
+    setTimeout(hidePopup, 500);
 }
 
-const showPopup = () =>{
+const showPopup = () => {
     popup1.setAttribute("visible", "true");
+}
+const hidePopup = () => {
+    popup1.setAttribute("visible", "false");
 }
