facf6a0bb20e1204f99679c46eca2c3cc91ea371
Tim <S1097097@student.hsleiden.nl>
2022-01-11 13:17:52 +0100

    Console log

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const desk2 = document.getElementById("desk2");
 
 const popup1 = document.getElementById("popup1");
 const popup2 = document.getElementById("popup2");
+const popup3 = document.getElementById("popup3");
 
 desk1.onmouseenter = () => {
     setTimeout(showPopup, 500);
@@ -11,15 +12,17 @@ desk1.onmouseleave = () => {
     setTimeout(hidePopup, 5000);
 }
 
-desk1.onmouseclick = () => {
-    setTimeout(hidePopup, 5000);
-}
-
 desk2.onmouseenter = () => {
     setTimeout(showPopup2, 500);
 }
 desk2.onmouseleave = () => {
     setTimeout(hidePopup2, 5000);
+}
+
+desk2.onclick = () => {
+    console.log("test");
+    popup2.setAttribute("visible", "false");
+    popup3.setAttribute("visible", "true");
 }
 
 const showPopup = () => {
@@ -33,4 +36,7 @@ const showPopup2 = () => {
 }
 const hidePopup2 = () => {
     popup2.setAttribute("visible", "false");
+}
+const showPopup3 = () => {
+    popup3.setAttribute("visible", "true");
 }
