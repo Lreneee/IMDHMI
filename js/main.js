540df8c41e57d35944a0d4cd07aa78bd0ee9a81c
Lreneee <irenedekoning1@gmail.com>
2022-01-11 12:15:46 +0100

    Updated

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 const desk1 = document.getElementById("desk1");
-const popup1 = document.getElementById("popup1");
+const desk2 = document.getElementById("desk2");
 
-let popup1Visible = false;
+const popup1 = document.getElementById("popup1");
+const popup2 = document.getElementById("popup2");
+
 desk1.onmouseenter = () => {
     setTimeout(showPopup, 500);
 }
 desk1.onmouseleave = () => {
-    setTimeout(hidePopup, 500);
+    setTimeout(hidePopup, 2000);
+}
+
+desk2.onmouseenter = () => {
+    setTimeout(showPopup2, 500);
+}
+desk2.onmouseleave = () => {
+    setTimeout(hidePopup2, 2000);
 }
 
 const showPopup = () => {
@@ -14,4 +23,10 @@ const showPopup = () => {
 }
 const hidePopup = () => {
     popup1.setAttribute("visible", "false");
+}
+const showPopup2 = () => {
+    popup2.setAttribute("visible", "true");
+}
+const hidePopup2 = () => {
+    popup2.setAttribute("visible", "false");
 }
