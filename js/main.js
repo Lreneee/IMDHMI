5c7fe733f076b2c40b41ab8f00ec3b0befc7a0f2
Lreneee <irenedekoning1@gmail.com>
2022-01-23 12:30:56 +0100

    Uitbreiding

--- js/main.js
+++ js/main.js
@@ -5,22 +5,40 @@ const popup1 = document.getElementById("popup1");
 const popup2 = document.getElementById("popup2");
 const popup3 = document.getElementById("popup3");
 const popup4 = document.getElementById("popup4");
+const popup5 = document.getElementById("popup5");
 
+const agenda_tim = document.getElementById("agenda_tim");
+const agenda_werkplek = document.getElementById("agenda_werkplek");
+
+//Bureau links
 desk1.onmouseenter = () => {
     setTimeout(showPopup, 5);
 }
-
 desk1.onmouseleave = () => {
     setTimeout(hidePopup, 5000);
-    setTimeout(hidePopup4, 1000);
 }
 
-//De agenda popup komt na 6 seconden omhoog
-desk1.onclick = () => {
-    setTimeout(showPopup4, 6000);
-    setTimeout(hidePopup, 6000);
+//Agenda Tim inzien
+agenda_tim.onmouseenter = () => {
+    console.log("touched 1")
+    setTimeout(showPopup5, 1000);
+    setTimeout(hidePopup, 999);
+}
+agenda_tim.onmouseleave = () => {
+    setTimeout(hidePopup5, 5000);
 }
 
+//Agenda werkplek inzien
+agenda_werkplek.onmouseenter = () => {
+    console.log("touched 2")
+    setTimeout(showPopup4, 1000);
+    setTimeout(hidePopup, 999);
+}
+agenda_werkplek.onmouseleave = () => {
+    setTimeout(hidePopup4, 5000);
+}
+
+//Bureau rechts
 desk2.onmouseleave = () => {
     setTimeout(hidePopup, 5000);
 }
@@ -31,20 +49,27 @@ desk2.onmouseenter = () => {
 desk2.onmouseleave = () => {
     setTimeout(hidePopup2, 7000);
 }
-
-//De reserveer popup komt na 6 seconden omhoog
 desk2.onclick = () => {
     console.log("test")
     setTimeout(showPopup3, 6000);
     setTimeout(hidePopup3, 10000);
 
 }
+popup2.onclick = () => {
+    console.log("test")
+    setTimeout(showPopup3, 6000);
+    setTimeout(hidePopup3, 10000);
+}
 
 const showPopup = () => {
     popup1.setAttribute("visible", "true");
+    agenda_tim.setAttribute("visible", "true");
+    agenda_werkplek.setAttribute("visible", "true");
 }
 const hidePopup = () => {
     popup1.setAttribute("visible", "false");
+    agenda_tim.setAttribute("visible", "false");
+    agenda_werkplek.setAttribute("visible", "false");
 }
 const showPopup2 = () => {
     popup2.setAttribute("visible", "true");
@@ -63,4 +88,10 @@ const showPopup4 = () => {
 }
 const hidePopup4 = () => {
     popup4.setAttribute("visible", "false");
+}
+const showPopup5 = () => {
+    popup5.setAttribute("visible", "true");
+}
+const hidePopup5 = () => {
+    popup5.setAttribute("visible", "false");
 }
